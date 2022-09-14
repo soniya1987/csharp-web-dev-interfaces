477887d3838cc86f80685a360dd09ec4037cb167
Soniya Vakkat Kuttan <vksoniya68@gmail.com>
2022-09-14 02:11:31 -0500

    Excercises: Interfaces and Polymorphism

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -13,11 +13,41 @@ namespace IceCreamShop
 
         // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+      //  FlavorComparer comparer = new FlavorComparer();
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            Console.WriteLine("Flavors Before:"+ "\n");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+          //  availableFlavors.Sort(comparer);
+          availableFlavors.Sort(new FlavorComparer());
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+            Console.WriteLine("\nCones Before:" + "\n");
+            foreach (Cone cones in availableCones)
+            {
+                Console.WriteLine(cones.Name +": " +cones.Cost);
+            }
+            availableCones.Sort(new ConeComparer());
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            Console.WriteLine("\nFlavors After:" + "\n");
+
+
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+
+            Console.WriteLine("\nCones After:" + "\n");
+            foreach (Cone cones in availableCones)
+            {
+                Console.WriteLine(cones.Name + ": " + cones.Cost);
+            }
+
         }
     }
 }
